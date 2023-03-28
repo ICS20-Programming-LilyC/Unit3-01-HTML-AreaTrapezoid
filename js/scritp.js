71a504d321db3034b8b64030c77b22d696502d2b
Replit user <>
2023-03-28 15:07:20 +0000

    Added index.html code

---
+++ js/scritp.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2023 Lily C All rights reserved
+//
+// Created by: Lily C
+// Created on: 03/28/2023
+// This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates the area of the triangle.
+ */
